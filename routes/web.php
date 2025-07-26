bd035b871b7de4908e2e84c017a956d28a87942a
Sakeerin <theblue0800@gmail.com>
2025-07-26 14:03:41 +0700

    Implement upvote and downvote

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::middleware('auth')->group(function () {
         })->name('dashboard');
 
         Route::resource('feature', FeatureController::class);
+
+        Route::post('/feature/{feature}/upvote', [\App\Http\Controllers\UpvoteController::class, 'store'])->name('upvote.store');
+        Route::delete('/upvote/{feature}', [\App\Http\Controllers\UpvoteController::class, 'destroy'])->name('upvote.destroy');
         
     });
 });
